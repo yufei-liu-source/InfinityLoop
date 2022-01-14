a40c5bcb52e59fb27677ede00d8aa3e8474c2c3d
liuyu <liuyufei9931@gmail.com>
2022-01-14 12:39:20 +0100

    first push

---
+++ loop/src/main/java/fr/dauphine/JavaAvance/Solve/Checker.java
@@ -0,0 +1,11 @@
+package fr.dauphine.JavaAvance.Solve;
+
+import fr.dauphine.JavaAvance.Components.Piece;
+import fr.dauphine.JavaAvance.GUI.Grid;
+
+
+public class Checker {
+
+// To be implemented
+	
+}
